dc111e6b409355e4fbe7a2c90c8bef069d8e99a3
Joosep Suuder <115637478+jsgngie@users.noreply.github.com>
2023-10-04 15:41:27 +0300

    login page tweaks
    login page button logic

--- js/login.js
+++ js/login.js
@@ -1,7 +1,20 @@
-function areFieldsEmpty() {
-    loginName = document.getElementById('loginName').value 
-    loginPassword = document.getElementById('loginPassword').value
-    if (loginName.length == 0 || loginPassword.length == 0) {
-        document.getElementById('errorMessage').style.visibility = 'visible'
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("loginFields");
+    const inputName = form.querySelector("input[name='loginName']");
+    const inputPass = form.querySelector("input[name='loginPassword']");
+    form.addEventListener("input", function (event) {
+        // Check if the event target is the input field
+        if (event.target === inputName || event.target === inputPass) {
+            if (inputName.value.length > 0 && inputPass.value.length > 0) {
+                document.getElementById("submit").disabled=false;
+            } else {
+                document.getElementById("submit").disabled=true;
+            }
+        }
+      });
+})
+
+function rememberIt() {
+    document.getElementById("loginBox").style.height="420px"
+    document.getElementById("toolTip").style.visibility="visible"
 }
