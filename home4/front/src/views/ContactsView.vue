455a2dc2446795cfaea0b0b8ce11aaf21bfd6fc7
unknown <evertsaarnak@gmail.com>
2023-12-14 12:09:14 +0200

    created Contacts page

---
+++ home4/front/src/views/ContactsView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="contacts">
+    <h1>Kontaktid</h1>
+    <p>Nimi: Evert email: evert@web.ee</p>
+    <p>Nimi: Joosep email: joosep@web.ee</p>
+    <p>Nimi: Triinu email: triinu@web.ee</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactsView",
+}
+</script>
